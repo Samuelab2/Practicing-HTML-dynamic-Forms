9e14a19a055df73de37666f7e17e805c4fbdfd5b
Samuel Mata <samuelab2@gmail.com>
2019-10-03 16:24:53 -0400

    making the inputs, selectors and texarea dynamic

--- media/js/main.js
+++ media/js/main.js
@@ -1,46 +1,68 @@
-// $(function () {
-// 	//-- function que va por ajax hacia el archivo datos.json y trae su contenido . y lo imprime por consola
-// 	function carga_de_datos() {
-// 		var datos = "";
-// 		$.ajax({
-// 			type: "GET",
-// 			url: "datos.json",
-// 			statusCode: {
-// 				404: function () {
-// 					console.log("Ups! ha ocurrido un inconveniente, function ");
-// 				}
-// 			}
-// 		}).done(function (resp) {
-// 			// console.log(resp);
-
-// 			for (var i = 0; i < resp.length; i++) {
-// 				$("#table").append(`<tr>
-// 				    										<td>${resp[i].nombres}</td>
-// 				    										<td>${resp[i].apellidos}</td>
-// 				    										<td>${resp[i].fecha_nacimiento}</td>
-// 	    												</tr>`);
-// 			}
-// 		});
-// 	}
-// 	carga_de_datos();
-// });	
-
 const form = document.getElementById("form")
 
 fetch('datos.json')
-	.then((resp) => resp.json())
-	.then((data) => {
-		// console.log(data);
+	.then(resp => resp.json())
+	.then(data => {
 		for (let e of data.datos_basicos) {
+
 			let label = document.createElement("label")
 			let br = document.createElement("br")
+			let br2 = document.createElement("br")
 			let input = document.createElement("input")
-			input.setAttribute("type", "text")
+
 			label.innerHTML = `${e.name}`
+
 			form.appendChild(label)
 			form.appendChild(br)
-			form.appendChild(input)
-			form.appendChild(br)
-			console.log(e.name);
+
+			switch (e.type) {
+				case "text":
+					input.setAttribute("type", `${e.type}`)
+					form.appendChild(input)
+					form.appendChild(br2)
+					break;
+				case "select":
+					let select = document.createElement("select")
+					select.setAttribute("id", `${e.name}`)
+					form.appendChild(select)
+					form.appendChild(br2)
+					break;
+				case "textarea":
+					let textarea = document.createElement("textarea")
+					form.appendChild(textarea)
+					form.appendChild(br2)
+					break;
+				case "date":
+					input.setAttribute("type", `${e.type}`)
+					form.appendChild(input)
+					form.appendChild(br2)
+					break;
+			}
 		}
 	})
+
+fetch('tipo_producto.json')
+	.then(resp => resp.json())
+	.then(data => {
+		console.log(data);
+		let option = document.createElement("option")
+		for (const key in data) {
+			if (data.hasOwnProperty(key)) {
+				console.log(data.hasOwnProperty(key));
+				const element = data[key];
+				console.log(element[0].name);
+			}
+		}
+		// for (const e of data.tipo_producto) {
+		// 	console.log(e.id);
+		// }
+		
+	})
+
+// fetch('modelo.json')
+// 	.then(resp => resp.json())
+// 	.then(data => console.log(data))
+
+// fetch('marca.json')
+// 	.then(resp => resp.json())
+// 	.then(data => console.log(data))
