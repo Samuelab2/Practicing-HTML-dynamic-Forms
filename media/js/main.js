01a0d7a8d2dbbdff342e9b508b71d684651a6cf3
Samuel Mata <samuelab2@gmail.com>
2019-10-04 16:24:37 -0400

    using promise.all to get all promises at the same time

--- media/js/main.js
+++ media/js/main.js
@@ -1,103 +1,116 @@
 const form = document.getElementById("form")
 
-fetch('datos.json')
-	.then(resp => resp.json())
-	.then(data => {
-		for (let e of data.datos_basicos) {
+const basicForm = arr => {
+	for (let e of arr) {
 
-			let label = document.createElement("label")
-			let br = document.createElement("br")
-			let br2 = document.createElement("br")
-			let input = document.createElement("input")
+		let label = document.createElement("label")
+		let br = document.createElement("br")
+		let br2 = document.createElement("br")
+		let input = document.createElement("input")
 
-			label.innerHTML = `${e.name}`
+		label.innerHTML = `${e.name}`
 
-			form.appendChild(label)
-			form.appendChild(br)
+		form.appendChild(label)
+		form.appendChild(br)
 
-			switch (e.type) {
-				case "text":
-					input.setAttribute("type", `${e.type}`)
-					form.appendChild(input)
-					form.appendChild(br2)
-					break;
-				case "select":
-					let select = document.createElement("select")
-					let option = document.createElement("option")
-					option.selected = true;
-					option.innerHTML = "Seleccione un item"
-					select.setAttribute("id", `${e.name}`)
-					select.appendChild(option)
-					form.appendChild(select)
-					form.appendChild(br2)
-					break;
-				case "textarea":
-					let textarea = document.createElement("textarea")
-					form.appendChild(textarea)
-					form.appendChild(br2)
-					break;
-				case "date":
-					input.setAttribute("type", `${e.type}`)
-					form.appendChild(input)
-					form.appendChild(br2)
-					break;
+		switch (e.type) {
+			case "text":
+				input.setAttribute("type", `${e.type}`)
+				form.appendChild(input)
+				form.appendChild(br2)
+				break;
+			case "select":
+				let select = document.createElement("select")
+				let option = document.createElement("option")
+				option.selected = true;
+				option.innerHTML = "Seleccione un item"
+				select.setAttribute("id", `${e.name}`)
+				select.appendChild(option)
+				form.appendChild(select)
+				form.appendChild(br2)
+				break;
+			case "textarea":
+				let textarea = document.createElement("textarea")
+				form.appendChild(textarea)
+				form.appendChild(br2)
+				break;
+			case "date":
+				input.setAttribute("type", `${e.type}`)
+				form.appendChild(input)
+				form.appendChild(br2)
+				break;
+		}
+	}
+}
+
+const selectTypeProducts = arr => {
+
+	for (const key in arr) {
+		if (arr.hasOwnProperty(key)) {
+			for (const i of arr[key]) {
+				let type_product_select = document.getElementById("Tipo Producto")
+				let option = document.createElement("option")
+				option.id = i.id
+				option.innerHTML = i.name
+				type_product_select.appendChild(option)
 			}
 		}
-	})
+	}
+}
 
-let product_type = [];
-let product_brand = [];
-let product_model = [];
-
-fetch('tipo_producto.json')
-	.then(resp => resp.json())
-	.then(data => {
-		for (const key in data) {
-			if (data.hasOwnProperty(key)) {
-				for (const i of data[key]) {
-					product_type.push(i)
-					let type_product_select = document.getElementById("Tipo Producto")
-					let option = document.createElement("option")
-					option.id = i.id
-					option.innerHTML = i.name
-					type_product_select.appendChild(option)
-				}
+const selectModelProducts = arr => {
+	for (const key in arr) {
+		if (arr.hasOwnProperty(key)) {
+			for (const i of arr[key]) {
+				let brand = document.getElementById("Marca")
+				let option = document.createElement("option")
+				option.id = i.id_tipo
+				option.innerHTML = i.name;
+				brand.appendChild(option)
 			}
 		}
-	})
+	}
+}
 
-fetch('modelo.json')
-	.then(resp => resp.json())
-	.then(data => {
-		for (const key in data) {
-			console.log(data);
-			if (data.hasOwnProperty(key)) {
-				for (const i of data[key]) {
-					product_brand.push(i)
-					let brand = document.getElementById("Marca")
-					let option = document.createElement("option")
-					option.id = i.id_tipo
-					option.innerHTML = i.name;
-					brand.appendChild(option)			
-				}
+const selectBrandProducts = arr => {
+	for (const key in arr) {
+		if (arr.hasOwnProperty(key)) {
+			for (const i of arr[key]) {
+				let model = document.getElementById("Modelo")
+				let option = document.createElement("option")
+				option.id = i.id_tipo
+				option.innerHTML = i.name;
+				model.appendChild(option)
 			}
 		}
-	})
+	}
+}
 
-fetch('marca.json')
-	.then(resp => resp.json())
-	.then(data => {
-		for (const key in data) {
-			if (data.hasOwnProperty(key)) {
-				for (const i of data[key]) {
-					product_model.push(i)
-					let model = document.getElementById("Modelo")
-					let option = document.createElement("option")
-					option.id = i.id_tipo
-					option.innerHTML = i.name;
-					model.appendChild(option)	
-				}
-			}
-		}
-	})
+let data = [
+	'datos.json',
+	'tipo_producto.json',
+	'marca.json',
+	'modelo.json'
+]
 
+let request = data.map(file => fetch(file))
+
+Promise.all(request)
+	// all responses are resolved successfully
+	.then(responses => responses)
+	// map array of responses into array of response.json() to read their content
+	// here we use promise.all again to get the response of every data
+	.then(responses => Promise.all(responses.map(r => r.json())))
+	.then(users => {
+		console.log(users[1].tipo_producto);
+
+		let datos_basicos = users[0].datos_basicos
+		let tipo_producto = users[1]
+		let marca = users[2]
+		let modelo = users[3]
+
+		basicForm(datos_basicos)
+		selectTypeProducts(tipo_producto)
+		selectModelProducts(marca)
+		selectBrandProducts(modelo)
+	})
