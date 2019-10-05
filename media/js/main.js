f87b10d71fd7a4b24ee4d85be7a064076fe2daee
Samuel Mata <Samuelab2@gmail.com>
2019-10-05 01:56:13 -0400

    agregando la validacion en los select

--- media/js/main.js
+++ media/js/main.js
@@ -1,8 +1,9 @@
 const form = document.getElementById("form")
 
 const basicForm = arr => {
-	for (let e of arr) {
 
+	for (let e of arr) {
+		
 		let label = document.createElement("label")
 		let br = document.createElement("br")
 		let br2 = document.createElement("br")
@@ -43,16 +44,18 @@ const basicForm = arr => {
 	}
 }
 
-const selectTypeProducts = arr => {
 
+const selectTypeProducts = arr => {
+	
 	for (const key in arr) {
 		if (arr.hasOwnProperty(key)) {
 			for (const i of arr[key]) {
 				let type_product_select = document.getElementById("Tipo Producto")
+				type_product_select.addEventListener("change", clear)
 				let option = document.createElement("option")
-				option.id = i.id
-				option.innerHTML = i.name
-				type_product_select.appendChild(option)
+				option.value = i.id
+				option.text = i.name
+				type_product_select.add(option)
 			}
 		}
 	}
@@ -64,7 +67,7 @@ const selectModelProducts = arr => {
 			for (const i of arr[key]) {
 				let brand = document.getElementById("Marca")
 				let option = document.createElement("option")
-				option.id = i.id_tipo
+				option.value = i.id_tipo
 				option.innerHTML = i.name;
 				brand.appendChild(option)
 			}
@@ -78,13 +81,17 @@ const selectBrandProducts = arr => {
 			for (const i of arr[key]) {
 				let model = document.getElementById("Modelo")
 				let option = document.createElement("option")
-				option.id = i.id_tipo
+				option.value = i.id_tipo
 				option.innerHTML = i.name;
 				model.appendChild(option)
 			}
 		}
 	}
 }
+
+let typeSelector;
+let brandSelector;
+let ModelSelector;
 
 let data = [
 	'datos.json',
@@ -96,21 +103,51 @@ let data = [
 let request = data.map(file => fetch(file))
 
 Promise.all(request)
-	// all responses are resolved successfully
-	.then(responses => responses)
-	// map array of responses into array of response.json() to read their content
-	// here we use promise.all again to get the response of every data
-	.then(responses => Promise.all(responses.map(r => r.json())))
-	.then(users => {
-		console.log(users[1].tipo_producto);
+// all responses are resolved successfully
+.then(responses => responses)
+// map array of responses into array of response.json() to read their content
+// here we use promise.all again to get the response of every data
+.then(responses => Promise.all(responses.map(r => r.json())))
+.then(users => {
+	
+	let datos_basicos = users[0].datos_basicos
+	let tipo_producto = users[1]
+	let marca = users[2]
+	let modelo = users[3]
+	
+	// console.log(tipo_producto);
+	
+	basicForm(datos_basicos)
+	selectTypeProducts(tipo_producto)
+	// selectModelProducts(marca)
+	// selectBrandProducts(modelo)
+	typeSelector = document.getElementById("Tipo Producto")
+	brandSelector = document.getElementById("Marca")
+	ModelSelector = document.getElementById("Modelo")
+})
 
-		let datos_basicos = users[0].datos_basicos
-		let tipo_producto = users[1]
-		let marca = users[2]
-		let modelo = users[3]
+function brand() {
+	fetch('marca.json')
+		.then(response => response.json())
+		.then(data => {
+			for (const key in data) {
+				for (const i of data[key]) {
+					let brand = document.getElementById("Marca")
+					let option = document.createElement("option")
+					if (i.id_tipo == typeSelector.value) {
+						option.text = i.name;
+						brand.add(option)
+					}
+				}
+			}
+		})
+}
 
-		basicForm(datos_basicos)
-		selectTypeProducts(tipo_producto)
-		selectModelProducts(marca)
-		selectBrandProducts(modelo)
-	})
+function clear() {
+	if (brandSelector.length > 1) {
+		for (let x = brandSelector.length; x >= 1; x--) {
+			brandSelector.remove(x)
+		}
+	}
+	brand()
+}
